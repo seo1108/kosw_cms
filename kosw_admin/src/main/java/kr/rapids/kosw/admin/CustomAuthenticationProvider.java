68576b7f85ad9351c82cdf1a6279a6962efc953e
SEO DONGWOON <seo1108@gmail.com>
2019-11-22 17:15:59 +0900

    로그인 모듈 수정

--- kosw_admin/src/main/java/kr/rapids/kosw/admin/CustomAuthenticationProvider.java
+++ kosw_admin/src/main/java/kr/rapids/kosw/admin/CustomAuthenticationProvider.java
@@ -33,7 +33,7 @@ public class CustomAuthenticationProvider implements AuthenticationProvider{
 		
 		Admin admin = new Admin();
 		admin.setEmail(username);
-		admin.setPasswd(password);
+		
 		
 		
 		Admin infoadmin = new Admin();
@@ -47,43 +47,113 @@ public class CustomAuthenticationProvider implements AuthenticationProvider{
 //		System.out.println(infoadmin.getPasswd());
 		
 		
-		BCryptPasswordEncoder bcrypt= new BCryptPasswordEncoder();  
-        boolean isPasswordMatches=bcrypt.matches(password, infoadmin.getPasswd());
-        if (isPasswordMatches) {
-        	System.out.println("1111111111111111111111111111111111111");
-        	admin.setPasswd(infoadmin.getPasswd());
-        	
-        	// 로그인 인증
-    		admin = adminService.adminLogin(admin);
-    		
-    		if (admin != null){
-    			if (!"Y".equals(admin.getActiveFlag())){
-    				throw new BadCredentialsException("관리자 승인대기 중입니다. 관리자에게 문의해주세요.");
-    			}
-    			List<String> roles = adminService.getRoles(admin); // "ROLE_SUPER" or "ROLE_NORMAL"
-    			
-    			List<GrantedAuthority> autoList = new ArrayList<GrantedAuthority>();
-    			
-    			for (Iterator iterator = roles.iterator(); iterator.hasNext();) {
-    				String role = (String) iterator.next();
-    				if ("ROLE_SUPER".equals(role)){
-    					admin.setSuperAdmin(true);
-    				}
-    				SimpleGrantedAuthority auth = new SimpleGrantedAuthority(role);
-    				autoList.add(auth);
-    			}
-
-    			UsernamePasswordAuthenticationToken usernamePasswordAuthenticationToken = new UsernamePasswordAuthenticationToken(username, password, autoList);
-    			usernamePasswordAuthenticationToken.setDetails(admin);
-    			return usernamePasswordAuthenticationToken;
-    		}else{
-    			throw new BadCredentialsException("아이디와 비밀번호를 확인해주세요.");
-    		}
-        } else {
-        	System.out.println("2222222222222222222222222222222222222");
-        	throw new BadCredentialsException("아이디와 비밀번호를 확인해주세요.");
-        }
 		
+		int adminseq = Integer.parseInt(infoadmin.getAdminSeq());
+		
+		
+		if (adminseq < 158) 
+		{
+			// 기존 사용자일 경우
+			admin.setPasswd(password);
+			admin = adminService.adminLogin(admin);
+			
+			if (admin != null){
+				if (!"Y".equals(admin.getActiveFlag())){
+					throw new BadCredentialsException("관리자 승인대기 중입니다. 관리자에게 문의해주세요.");
+				}
+				List<String> roles = adminService.getRoles(admin); // "ROLE_SUPER" or "ROLE_NORMAL"
+				
+				List<GrantedAuthority> autoList = new ArrayList<GrantedAuthority>();
+				
+				for (Iterator iterator = roles.iterator(); iterator.hasNext();) {
+					String role = (String) iterator.next();
+					if ("ROLE_SUPER".equals(role)){
+						admin.setSuperAdmin(true);
+					}
+					SimpleGrantedAuthority auth = new SimpleGrantedAuthority(role);
+					autoList.add(auth);
+				}
+	
+				UsernamePasswordAuthenticationToken usernamePasswordAuthenticationToken = new UsernamePasswordAuthenticationToken(username, password, autoList);
+				usernamePasswordAuthenticationToken.setDetails(admin);
+				return usernamePasswordAuthenticationToken;
+			}else{
+				throw new BadCredentialsException("아이디와 비밀번호를 확인해주세요.");
+			}
+		}
+		else 
+		{
+			// 신규 사용자는 암호화 로직
+			// 카카오톡 미사용자
+			if (!infoadmin.getEmail().equals(infoadmin.getPasswd()))
+			{
+				BCryptPasswordEncoder bcrypt= new BCryptPasswordEncoder();  
+		        boolean isPasswordMatches=bcrypt.matches(password, infoadmin.getPasswd());
+		        if (isPasswordMatches) {
+		        	admin.setPasswd(infoadmin.getPasswd());
+		        	
+		        	// 로그인 인증
+		    		admin = adminService.adminLogin(admin);
+		    		
+		    		if (admin != null){
+		    			if (!"Y".equals(admin.getActiveFlag())){
+		    				throw new BadCredentialsException("관리자 승인대기 중입니다. 관리자에게 문의해주세요.");
+		    			}
+		    			List<String> roles = adminService.getRoles(admin); // "ROLE_SUPER" or "ROLE_NORMAL"
+		    			
+		    			List<GrantedAuthority> autoList = new ArrayList<GrantedAuthority>();
+		    			
+		    			for (Iterator iterator = roles.iterator(); iterator.hasNext();) {
+		    				String role = (String) iterator.next();
+		    				if ("ROLE_SUPER".equals(role)) {
+		    					admin.setSuperAdmin(true);
+		    				}
+		    				SimpleGrantedAuthority auth = new SimpleGrantedAuthority(role);
+		    				autoList.add(auth);
+		    			}
+		
+		    			UsernamePasswordAuthenticationToken usernamePasswordAuthenticationToken = new UsernamePasswordAuthenticationToken(username, password, autoList);
+		    			usernamePasswordAuthenticationToken.setDetails(admin);
+		    			return usernamePasswordAuthenticationToken;
+		    		}else{
+		    			throw new BadCredentialsException("아이디와 비밀번호를 확인해주세요.");
+		    		}
+		        } else {
+		        	throw new BadCredentialsException("아이디와 비밀번호를 확인해주세요.");
+		        }
+			}
+			else 
+			// 카카오톡 연동 사용자의 경우, 비밀번호와 이메일이 같기 때문에, 평문 로직
+			{
+				// 기존 사용자일 경우
+				admin.setPasswd(password);
+				admin = adminService.adminLogin(admin);
+				
+				if (admin != null){
+					if (!"Y".equals(admin.getActiveFlag())){
+						throw new BadCredentialsException("관리자 승인대기 중입니다. 관리자에게 문의해주세요.");
+					}
+					List<String> roles = adminService.getRoles(admin); // "ROLE_SUPER" or "ROLE_NORMAL"
+					
+					List<GrantedAuthority> autoList = new ArrayList<GrantedAuthority>();
+					
+					for (Iterator iterator = roles.iterator(); iterator.hasNext();) {
+						String role = (String) iterator.next();
+						if ("ROLE_SUPER".equals(role)){
+							admin.setSuperAdmin(true);
+						}
+						SimpleGrantedAuthority auth = new SimpleGrantedAuthority(role);
+						autoList.add(auth);
+					}
+		
+					UsernamePasswordAuthenticationToken usernamePasswordAuthenticationToken = new UsernamePasswordAuthenticationToken(username, password, autoList);
+					usernamePasswordAuthenticationToken.setDetails(admin);
+					return usernamePasswordAuthenticationToken;
+				}else{
+					throw new BadCredentialsException("아이디와 비밀번호를 확인해주세요.");
+				}
+			}
+		}
 		
 //		if ( Util.isMatchPassword(new BCryptPasswordEncoder(16).encode(password), infoadmin.getPasswd())) {
 //			System.out.println("1111111111111111111111111111111111111");
